f8b7eadd4411e398914e3a83ae35faf2c8552ff1
hanifwidyantoro <hnfwidian@gmail.com>
2021-11-09 13:50:58 +0700

    penambahan role

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
-use Illuminate\Support\Facades\Gate
+use Illuminate\Support\Facades\Gate;
 
 
 class UserController extends Controller
